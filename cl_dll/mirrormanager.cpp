d9a1ade4210b6f1605aeb26eae2d12682e5152a7
Bacontsu <62763382+bacontsu@users.noreply.github.com>
2022-10-06 17:36:30 +0700

    Opacity support for reflective surfaces
    credit @BlueNightHawk

--- cl_dll/mirrormanager.cpp
+++ cl_dll/mirrormanager.cpp
@@ -453,7 +453,13 @@ void CMirrorManager::DrawMirrors( )
 		model_t *model = m_pMirrors[i].entity->model;
 		msurface_t *psurf = &model->surfaces[model->firstmodelsurface];
 
+		// BlueNightHawk - add opacity support for mirrors
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glColor4f(1.0f, 1.0f, 1.0f, (m_pCurrentMirror->entity->curstate.renderamt / 255.0f));
 		gBSPRenderer.DrawPolyFromArray(psurf->polys);
+		glDisable(GL_BLEND);
+
 		psurf->visframe = gBSPRenderer.m_iFrameCount;// For decals
 		
 		glPopMatrix();
